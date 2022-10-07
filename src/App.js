98fb8109bf9b78c4987ec3adcfce85eb20db48d5
fahrettinbyrm <git.fahrettin@gmail.com>
2022-10-07 19:37:15 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Todo from './components/todo';
+
+function App() {
+  return (
+    <div className='container border border-primary rounded mt-5'
+    style={{
+     backgroundColor:"revert-layer"
+    }}
+    >
+      <Todo/>
+
+    </div>
+  );
+}
+
+export default App;
